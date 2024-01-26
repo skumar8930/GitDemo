85b73ef7e826a9a3027780df54c40e173a5483fe
skumar8930 <skumar8930@gmail.com>
2024-01-26 17:10:39 +0530

    Anil changes

--- src/main/java/SandeepClass.java
+++ src/main/java/SandeepClass.java
@@ -1,5 +1,6 @@
 public class SandeepClass {
-    public static void main(String[] arr){
-        System.out.println("Sandeep change1");
+    public static void main(String [] arr){
+        System.out.println("Anil changes");
+
     }
 }
