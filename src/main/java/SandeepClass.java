109036b120e6658aaf9bb3f468b4a33b5db2355c
skumar8930 <skumar8930@gmail.com>
2024-01-26 17:28:30 +0530

    Sandeep changes3

--- src/main/java/SandeepClass.java
+++ src/main/java/SandeepClass.java
@@ -1,5 +1,9 @@
 public class SandeepClass {
     public static void main(String [] arr){
         System.out.println("Sandeep changes");
+        System.out.println("Sandeep change2");
+        System.out.println("Sandeep change3");
+        System.out.println("Sandeep changes4");
+
     }
 }
