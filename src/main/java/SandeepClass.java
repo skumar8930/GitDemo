c8b5ba73aece5aa9d531a4087d95abfefc5df60e
skumar8930 <skumar8930@gmail.com>
2024-01-28 20:57:26 +0530

    Anil commits

--- src/main/java/SandeepClass.java
+++ src/main/java/SandeepClass.java
@@ -1,6 +1,14 @@
 public class SandeepClass {
     public static void main(String [] arr){
    System.out.print(" Anil commets 1");
+        System.out.print(" Anil commets 1");
+        System.out.print(" Anil commets 2");
+        System.out.print(" Anil commets 3");
+        System.out.print(" Anil commets 4");
+        System.out.print(" Anil commets 5");
+        System.out.print(" Anil commets 6");
+
+
 
     }
 }
