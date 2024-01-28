a24c42f187dd14b417b1ceecf33977e33eb46097
skumar8930 <skumar8930@gmail.com>
2024-01-28 20:41:12 +0530

    master change2

--- src/main/java/SandeepClass.java
+++ src/main/java/SandeepClass.java
@@ -1,11 +1,6 @@
 public class SandeepClass {
     public static void main(String [] arr){
-        System.out.println("Sandeep change");
-        System.out.println("master change");
 
-        System.out.println("Sandeep change2");
-        System.out.println("Sandeep change3");
-        System.out.println("master change2");
 
     }
 }
